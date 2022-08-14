b0d56e6c140853df2e0aa48e6e2c9526e8d517c5
Michael Savin <jtprogru@gmail.com>
2022-08-14 17:35:19 +0300

    Update template
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 ```shell
 export BOT_TOKEN="1234567890:VCDRJHGYUIKMNBGYUIKMNBvfr45678iuyhgtfr567u"
 export DEBUG=1
+export SERVE_PORT=8000
+export SERVE_HOST=127.0.0.1
 ```
 
 Выполнить:
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,8 +11,13 @@ func main() {
 		DisableColors:   true,
 		TimestampFormat: "2006-01-02T15:04:05",
 		FullTimestamp:   true,
+		ForceQuote:      true,
 	}
 	logger.Out = os.Stdout
 	logger.SetReportCaller(true)
 	logger.Info("bot is started...")
+	botToken := os.Getenv("OWL_BOT_TOKEN")
+	serveHost := os.Getenv("OWL_SERVE_HOST")
+	servePort := os.Getenv("OWL_SERVE_PORT")
+	logger.Infof("serve on host %v:%v with token %v", serveHost, servePort, botToken)
 }
